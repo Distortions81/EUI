d0cba9e9d659f51796e9841b87d15b6df873ea94
Distortions81 <carlotto81@gmail.com>
2025-01-12 19:20:07 -0700

    Handy list of colors

---
+++ colors.go
@@ -0,0 +1,64 @@
+package main
+
+import "image/color"
+
+var (
+	ColorWhite   = color.RGBA{255, 255, 255, 255}
+	ColorBlack   = color.RGBA{0, 0, 0, 255}
+	ColorRed     = color.RGBA{203, 67, 53, 255}
+	ColorGreen   = color.RGBA{40, 180, 99, 255}
+	ColorBlue    = color.RGBA{41, 128, 185, 255}
+	ColorYellow  = color.RGBA{244, 208, 63, 255}
+	ColorGray    = color.RGBA{128, 128, 128, 255}
+	ColorOrange  = color.RGBA{243, 156, 18, 255}
+	ColorPink    = color.RGBA{255, 151, 197, 255}
+	ColorPurple  = color.RGBA{165, 105, 189, 255}
+	ColorSilver  = color.RGBA{209, 209, 209, 255}
+	ColorTeal    = color.RGBA{64, 199, 178, 255}
+	ColorMaroon  = color.RGBA{199, 54, 103, 255}
+	ColorNavy    = color.RGBA{99, 114, 166, 255}
+	ColorOlive   = color.RGBA{134, 166, 99, 255}
+	ColorLime    = color.RGBA{206, 231, 114, 255}
+	ColorFuchsia = color.RGBA{209, 114, 231, 255}
+	ColorAqua    = color.RGBA{114, 228, 231, 255}
+	ColorBrown   = color.RGBA{176, 116, 78, 255}
+	ColorRust    = color.RGBA{210, 113, 52, 255}
+
+	ColorLightRed     = color.RGBA{255, 83, 83, 255}
+	ColorLightGreen   = color.RGBA{170, 255, 159, 255}
+	ColorLightBlue    = color.RGBA{159, 186, 255, 255}
+	ColorLightYellow  = color.RGBA{255, 251, 159, 255}
+	ColorLightGray    = color.RGBA{236, 236, 236, 255}
+	ColorLightOrange  = color.RGBA{252, 213, 134, 255}
+	ColorLightPink    = color.RGBA{254, 163, 182, 255}
+	ColorLightPurple  = color.RGBA{254, 163, 245, 255}
+	ColorLightSilver  = color.RGBA{228, 228, 228, 255}
+	ColorLightTeal    = color.RGBA{152, 221, 210, 255}
+	ColorLightMaroon  = color.RGBA{215, 124, 143, 255}
+	ColorLightNavy    = color.RGBA{128, 152, 197, 255}
+	ColorLightOlive   = color.RGBA{186, 228, 144, 255}
+	ColorLightLime    = color.RGBA{219, 243, 153, 255}
+	ColorLightFuchsia = color.RGBA{239, 196, 253, 255}
+	ColorLightAqua    = color.RGBA{196, 246, 253, 255}
+
+	ColorDarkRed      = color.RGBA{146, 22, 22, 255}
+	ColorDarkGreen    = color.RGBA{22, 146, 24, 255}
+	ColorDarkBlue     = color.RGBA{22, 98, 146, 255}
+	ColorDarkYellow   = color.RGBA{139, 146, 22, 255}
+	ColorDarkGray     = color.RGBA{111, 111, 111, 255}
+	ColorCharcoal     = color.RGBA{16, 16, 16, 255}
+	ColorCharcoalSemi = color.RGBA{16, 16, 16, 128}
+	ColorDarkOrange   = color.RGBA{175, 117, 32, 255}
+	ColorDarkPink     = color.RGBA{128, 64, 64, 255}
+	ColorDarkPurple   = color.RGBA{137, 32, 175, 255}
+	ColorDarkSilver   = color.RGBA{162, 162, 162, 255}
+	ColorDarkTeal     = color.RGBA{27, 110, 86, 255}
+	ColorDarkMaroon   = color.RGBA{110, 27, 55, 255}
+	ColorDarkNavy     = color.RGBA{16, 46, 85, 255}
+	ColorDarkOlive    = color.RGBA{60, 101, 19, 255}
+	ColorDarkLime     = color.RGBA{122, 154, 45, 255}
+	ColorDarkFuchsia  = color.RGBA{154, 45, 141, 255}
+	ColorDarkAqua     = color.RGBA{45, 154, 154, 255}
+
+	ColorVeryDarkGray = color.RGBA{64, 64, 64, 255}
+)
