f9f39b06b549c9778e748452aaa6ef6a9d471a9d
Carl Otto <33408758+Distortions81@users.noreply.github.com>
2025-07-14 00:17:45 -0600

    Apply theme immediately and make theme window movable

--- theme_selector.go
+++ theme_selector.go
@@ -13,14 +13,13 @@ func makeThemeSelector() *windowData {
 	}
 	win := NewWindow(&windowData{
 		Title:     "Themes",
-		PinTo:     PIN_TOP_RIGHT,
-		Movable:   false,
-		Resizable: false,
-		Closable:  false,
+		Movable:   true,
+		Resizable: true,
+		Closable:  true,
 		// Give the dropdown room to fully render by accounting for the
 		// title bar height and the control's size.
 		Size:     point{X: 192, Y: 160},
-		Position: point{X: 4, Y: 4},
+		Position: point{X: float32(screenWidth) - 200, Y: 4},
 		Open:     true,
 	})
 	dd := NewDropdown(&itemData{Size: point{X: 150, Y: 24}, FontSize: 8})
