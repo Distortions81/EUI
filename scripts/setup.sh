18ed03c399720d26c19ad876c7575e8620fca388
Distortions81 <carlotto81@gmail.com>
2025-07-19 06:58:59 -0600

    .

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,3 +2,5 @@
 set -e
 sudo apt-get update
 sudo apt-get install -y libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev libxxf86vm-dev libgl1-mesa-dev
+sudo aptitude -f install build-essential
+sudo apt install libc6-dev libgl1-mesa-dev libxcursor-dev libxi-dev libxinerama-dev libxrandr-dev libxxf86vm-dev libasound2-dev pkg-config
