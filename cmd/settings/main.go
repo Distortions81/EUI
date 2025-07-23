73a931294eca16b7d2884c7260f151f49c3f7465
Distortions81 <carlotto81@gmail.com>
2025-07-23 09:58:51 -0600

    .

--- cmd/settings/main.go
+++ cmd/settings/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
@@ -15,7 +16,6 @@ import (
 var (
 	debugMode    *bool
 	dumpMode     *bool
-	themeSel     *eui.WindowData
 	signalHandle chan os.Signal
 	currentScale float32
 )
@@ -43,16 +43,67 @@ func main() {
 	mainFlow := &eui.ItemData{
 		ItemType: eui.ITEM_FLOW,
 		Size:     win.Size,
-		FlowType: eui.FLOW_HORIZONTAL,
+		FlowType: eui.FLOW_VERTICAL,
 	}
 	win.AddItem(mainFlow)
 
-	b1, _ := eui.NewButton(&eui.ItemData{Text: "Setting A", Size: eui.Point{X: 100, Y: 24}})
+	b1, _ := eui.NewCheckbox(&eui.ItemData{Text: "Show Item Names", Size: eui.Point{X: 150, Y: 24}})
 	mainFlow.AddItem(b1)
-	b2, _ := eui.NewButton(&eui.ItemData{Text: "Setting B", Size: eui.Point{X: 100, Y: 24}})
+	b2, _ := eui.NewCheckbox(&eui.ItemData{Text: "Show Legends", Size: eui.Point{X: 150, Y: 24}})
 	mainFlow.AddItem(b2)
-	b3, _ := eui.NewButton(&eui.ItemData{Text: "Setting C", Size: eui.Point{X: 100, Y: 24}})
+	b3, _ := eui.NewCheckbox(&eui.ItemData{Text: "Use Item Numbers", Size: eui.Point{X: 150, Y: 24}})
 	mainFlow.AddItem(b3)
+
+	IconFlow := &eui.ItemData{
+		ItemType:   eui.ITEM_FLOW,
+		Size:       eui.Point{X: 150, Y: 24},
+		FlowType:   eui.FLOW_HORIZONTAL,
+		Fixed:      true,
+		Scrollable: false,
+	}
+	mainFlow.AddItem(IconFlow)
+
+	t1, _ := eui.NewText(&eui.ItemData{Text: "Icon Size:", FontSize: 9, Size: eui.Point{X: 50, Y: 24}})
+	IconFlow.AddItem(t1)
+	t2, _ := eui.NewButton(&eui.ItemData{Text: "-", Size: eui.Point{X: 16, Y: 16}})
+	IconFlow.AddItem(t2)
+	t3, _ := eui.NewButton(&eui.ItemData{Text: "+", Size: eui.Point{X: 16, Y: 16}})
+	IconFlow.AddItem(t3)
+
+	ScaleFlow := &eui.ItemData{
+		ItemType:   eui.ITEM_FLOW,
+		Size:       eui.Point{X: 150, Y: 24},
+		FlowType:   eui.FLOW_HORIZONTAL,
+		Fixed:      true,
+		Scrollable: false,
+	}
+	mainFlow.AddItem(ScaleFlow)
+
+	u1, _ := eui.NewText(&eui.ItemData{Text: "UI Scale:", FontSize: 9, Size: eui.Point{X: 50, Y: 24}})
+	ScaleFlow.AddItem(u1)
+	u2, _ := eui.NewButton(&eui.ItemData{Text: "-", Size: eui.Point{X: 16, Y: 16}})
+	ScaleFlow.AddItem(u2)
+	u3, _ := eui.NewButton(&eui.ItemData{Text: "+", Size: eui.Point{X: 16, Y: 16}})
+	ScaleFlow.AddItem(u3)
+
+	c1, _ := eui.NewCheckbox(&eui.ItemData{Text: "Textures", Size: eui.Point{X: 150, Y: 24}})
+	mainFlow.AddItem(c1)
+	c2, _ := eui.NewCheckbox(&eui.ItemData{Text: "VSync", Size: eui.Point{X: 150, Y: 24}})
+	mainFlow.AddItem(c2)
+	c3, _ := eui.NewCheckbox(&eui.ItemData{Text: "Power Saver", Size: eui.Point{X: 150, Y: 24}})
+	mainFlow.AddItem(c3)
+	c4, _ := eui.NewCheckbox(&eui.ItemData{Text: "Linear Filtering", Size: eui.Point{X: 150, Y: 24}})
+	mainFlow.AddItem(c4)
+	c5, _ := eui.NewCheckbox(&eui.ItemData{Text: "HiDPI", Size: eui.Point{X: 150, Y: 24}})
+	mainFlow.AddItem(c5)
+
+	cFPS := fmt.Sprintf("FPS: %2.2f", ebiten.ActualFPS())
+	tt1, _ := eui.NewText(&eui.ItemData{Text: cFPS, FontSize: 9, Size: eui.Point{X: 150, Y: 12}})
+	mainFlow.AddItem(tt1)
+
+	vers := fmt.Sprintf("Version: %v", "v0.0.9-012345")
+	tt2, _ := eui.NewText(&eui.ItemData{Text: vers, FontSize: 9, Size: eui.Point{X: 150, Y: 12}})
+	mainFlow.AddItem(tt2)
 
 	win.AddWindow(false)
 	go startEbiten()
