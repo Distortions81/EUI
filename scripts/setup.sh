33787e198e5d887bf37ed9e77d8034a137c874db
Carl Otto <33408758+Distortions81@users.noreply.github.com>
2025-08-09 21:36:17 -0600

    Use apt-get with noninteractive installs

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,5 +2,5 @@
 set -e
 sudo apt-get update
 sudo apt-get install -y libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev libxxf86vm-dev libgl1-mesa-dev brotli
-sudo aptitude -f install build-essential
-sudo apt install libc6-dev libgl1-mesa-dev libxcursor-dev libxi-dev libxinerama-dev libxrandr-dev libxxf86vm-dev libasound2-dev pkg-config
+sudo apt-get install -y build-essential
+sudo apt-get install -y libc6-dev libgl1-mesa-dev libxcursor-dev libxi-dev libxinerama-dev libxrandr-dev libxxf86vm-dev libasound2-dev pkg-config
